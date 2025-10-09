e0e532bfbea84a80075cfa5064193d73e020f006
Hubert <hubert.wojcik002@gmail.com>
2025-10-09 10:44:15 +0200

    dvc pipeline work

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -1,8 +1,11 @@
+import sys
 from huggingface_hub import snapshot_download
+
+local_dir = sys.argv[1] if len(sys.argv) > 1 else "./data/raw/food101"
 
 snapshot_download(
     repo_id="ethz/food101",
     repo_type="dataset",
-    local_dir="./data/raw/food101",
+    local_dir=local_dir,
     local_dir_use_symlinks=False,  # ensures actual files, not symlinks
 )
